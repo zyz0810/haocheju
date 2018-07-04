62489de9112c30e7a70f935e00be06a1ab853ac6
郑艳子 <zhengyanzi@tiaohuo.com>
2018-07-04 11:06:27 +0800

    源码

---
+++ src/js/service.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jf on 2015/9/11.
+ * Modified by bear on 2016/9/7.
+ */
+@import "../../js/service/cookie.js";
+// @import "../../js/service/member.js";
+@import "../../js/service/first.js";
+@import "../../js/service/zixun.js";
+@import "../../js/service/personnel.js";
