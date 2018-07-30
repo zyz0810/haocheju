028153feb8f1141ab16c425392b29f4356392f66
郑艳子 <zhengyanzi@tiaohuo.com>
2018-07-30 16:17:03 +0800

    收藏、取消收藏  （车详情）

--- src/js/service/cars.js
+++ src/js/service/cars.js
@@ -50,6 +50,30 @@ cars.prototype = {
         });
     },
     /**
+     * 收藏车辆
+     * @param userId 用户Id
+     * @param productId  车Id
+     * @param type  1、新车 2、二手车
+     */
+    favorite: function (data) {
+        ajax.post({
+            url: base + "Api/Collect/pullcollect",
+            data: data,
+            success: this.fn
+        });
+    },
+    /**
+     * 取消收藏车辆
+     * @param 	cid 收藏ID
+     */
+    delFavorite: function (data) {
+        ajax.post({
+            url: base + "Api/Collect/cancelcollect",
+            data: data,
+            success: this.fn
+        });
+    },
+    /**
      * 拼车列表
      * @param page  页码
      * @param type  1、车找人 2人找车
