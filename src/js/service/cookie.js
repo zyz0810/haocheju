44a8edd7163f9dfac1b21283da11504864e9e45e
郑艳子 <zhengyanzi@tiaohuo.com>
2018-08-07 10:32:39 +0800

    cookie

--- src/js/service/cookie.js
+++ src/js/service/cookie.js
@@ -9,10 +9,13 @@ var cookie = {
      * @param value
      * @param expiredays
      */
-    setCookie: function (c_name, value, expiredays) {
+    setCookie: function (c_name, value, expiredays, path) {
         var exdate = new Date();
         exdate.setDate(exdate.getDate() + expiredays);
-        document.cookie = c_name + "=" + escape(value) + ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString());
+        document.cookie = c_name + "=" + escape(value) + ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString()) + ";path=" + path;
+
+        console.log(c_name + "=" + escape(value) + ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString()) + ";path=" + path)
+
     },
     /**
      * 读取cookie
@@ -21,10 +24,10 @@ var cookie = {
      */
     getCookie: function (c_name) {
         if (document.cookie.length > 0) {
-            c_start = document.cookie.indexOf(c_name + "=");
+            var c_start = document.cookie.indexOf(c_name + "=");
             if (c_start != -1) {
                 c_start = c_start + c_name.length + 1;
-                c_end = document.cookie.indexOf(";", c_start);
+                var c_end = document.cookie.indexOf(";", c_start);
                 if (c_end == -1) c_end = document.cookie.length;
                 return unescape(document.cookie.substring(c_start, c_end));
             }
